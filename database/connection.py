b72260bb183127828dc9d8d09c6a68ab533f138e
Josue Guevara <guevarajosue637@gmail.com>
2024-03-12 18:53:57 -0600

    Configuraciones para conexion de base de datos

---
+++ database/connection.py
@@ -0,0 +1,16 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://guevarajosue637:v8FzPfzWkj5a7Pnj@tigerocr.z8x5i0u.mongodb.net/?retryWrites=true&w=majority&appName=TigerOCR"
+
+client = MongoClient(uri, server_api=ServerApi("1"))
+
+
+def get_Connection():
+    try:
+        client.admin.command("ping")
+        print("Connected to MongoDB")
+    except Exception as e:
+        print("Not connected to MongoDB")
+        print(e)
+        
